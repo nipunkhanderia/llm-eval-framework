17528d68b534cdd7d3f7d84ec7b707024cd20f6e
nipunkhanderia <nipunkhanderia@gmail.com>
2026-05-19 22:08:21 +0000

    ML data added with fitting

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
 
-melbourne_file_path = r"C:\Users\Nipun\Downloads\melbourne_data.csv"
+melbourne_file_path = r"C:\Users\Nipun\Downloads\melbourne_dataz.csv"
 melbourne_dataz = pd.read_csv(melbourne_file_path)
 
-print(melbourne_dataz.describe())
+y = melbourne_dataz.SalePrice
+print(y)
 
+melbourne_features = ["LotArea","YearBuilt","1stFlrSF","2ndFlrSF","FullBath","BedroomAbvGr","TotRmsAbvGrd"]
+
+X = melbourne_dataz[melbourne_features]
+print(X.describe())
 
