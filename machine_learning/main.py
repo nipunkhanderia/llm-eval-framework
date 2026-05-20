2f1a4a0d1f12b89437708a378faab5087456ae53
nipunkhanderia <nipunkhanderia@gmail.com>
2026-05-20 08:25:48 +0000

    data set changed to proper raw data

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
 
-melbourne_file_path = r"C:\Users\Nipun\Downloads\melbourne_dataz.csv"
+melbourne_file_path = r"C:\Users\Nipun\Downloads\llm-eval-frameworkz\machine_learning\train.csv"
 melbourne_dataz = pd.read_csv(melbourne_file_path)
+
+
+# print(melbourne_dataz.describe())
+
 
 y = melbourne_dataz.SalePrice
 print(y)
