066c29183bcbcc0c2a1d1f6a7a9e46715dad7257
nipunkhanderia <nipunkhanderia@gmail.com>
2026-05-23 18:14:24 +0000

    pytest fixtures added

--- pytest/test_bank_details.py
+++ pytest/test_bank_details.py
@@ -1,6 +1,7 @@
 import pytest
 from bank_details import create_account
 from bank_details import search_account
+from bank_details import account_list
 
 def test_create_account():
     accntlist = create_account("Nipun", 100)
@@ -13,3 +14,18 @@ def test_search():
     result = search_account("Stark")
     assert result == "Stark", "name not found"
 
+
+
+@pytest.fixture
+def set_up_acount():
+    create_account("Nipun", 100)
+    create_account("Rhythm", 200)
+    return account_list
+
+def test_search_function(set_up_acount):
+    result = search_account("Nipun")
+    assert result == "Nipun"
+
+def Test_create_Acount(set_up_acount):
+        acc = account_list[0]
+        assert acc["name"] == "Nipun", "Name not found people"
